ba4d3c53da29b150c3001a1752344800424274f0
sssr33 <sskr33@gmail.com>
2016-05-07 23:55:59 +0300

    Added w81 and wp81 apps.

--- StarskyW81/StarskyW81.Windows/MainPage.xaml.cs
+++ StarskyW81/StarskyW81.Windows/MainPage.xaml.cs
@@ -12,11 +12,10 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using StarskyMain;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace Starsky
+namespace StarskyW81
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
